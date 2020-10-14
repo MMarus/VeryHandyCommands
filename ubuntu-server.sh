9c20716ad0e85a84be1c59863369021b0ee4ea92
Marek Marusic <MMarus@users.noreply.github.com>
2020-10-14 20:47:28 +0200

    Update ubuntu-server.sh

--- ubuntu-server.sh
+++ ubuntu-server.sh
@@ -1,1 +1,5 @@
+# do not wait while the network comes online during boot
+systemctl disable systemd-networkd-wait-online.service
+systemctl mask systemd-networkd-wait-online.service 
+
 echo "$user ALL=(ALL) NOPASSWD:ALL" | sudo tee -a /etc/sudoers.d/90-cloudimg-ubuntu #passwordless sudo
